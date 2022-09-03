a7607e8a19032df193718d50d491f3e667b15741
Bradley Yeo <yeo.bradley@gmail.com>
2022-09-03 14:00:58 +0800

    add data source to refactor iam policies

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,6 +12,67 @@ variable "lambda_role" {
 
 variable "force_destroy" {
   description = "Force deleting a resource"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
+}
+
+data "aws_iam_policy_document" "lambda_policy" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      # S3 policies
+      "s3:DeleteObject",
+      "s3:ListBucket",
+      "s3:HeadObject",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:PutObject",
+      # add DynamoDB policies
+      "dynamodb:BatchGetItem",
+      "dynamodb:GetItem",
+      "dynamodb:Query",
+      "dynamodb:Scan",
+      "dynamodb:BatchWriteItem",
+      "dynamodb:PutItem",
+      "dynamodb:UpdateItem"
+    ]
+    resources = ["arn:aws:s3:::*", "arn:aws:dynamodb:::*", "arn:aws:logs:::*"]
+    effect    = "Allow"
+  }
+}
+
+data "iam_policy_document" "developer_policy" {
+  statement {
+    actions = ["rds:*",
+      "cloudtrail:*",
+      "dynamodb:*",
+      "sqs:*",
+      "rolesanywhere:*",
+      "cloudfront:*",
+      "access-analyzer:*",
+      "kms:*",
+      "kinesis:*",
+      "events:*",
+      "sns:*",
+      "states:*",
+      "rds-db:*",
+      "cognito-identity:*",
+      "dax:*",
+      "s3:*",
+      "apigateway:*",
+      "rds-data:*",
+      "amplifybackend:*",
+      "appsync:*",
+      "sts:*",
+      "iam:*",
+      "cloudwatch:*",
+      "sso:*",
+      "lambda:*",
+      "route53:*",
+    "cognito-idp:*"]
+    resources = "*"
+    effect    = "Allow"
+  }
 }
