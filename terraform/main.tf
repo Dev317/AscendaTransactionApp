be5389baff49c83842240e1b5baae4d62811e1bc
Bradley Yeo <yeo.bradley@gmail.com>
2022-09-06 12:38:43 +0800

    uncomment iam user creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,58 +3,58 @@ provider "aws" {
   region = var.aws_region
 }
 # Create an archive file for the Python lambda package
-# data "archive_file" "python_lambda_package" {
-#   type        = "zip"
-#   source_file = "/code/handler.py"
-#   output_path = "/build/handler.zip"
-# }
+data "archive_file" "python_lambda_package" {
+  type        = "zip"
+  source_file = "/code/handler.py"
+  output_path = "/build/handler.zip"
+}
 
-# resource "aws_iam_group" "developers" {
-#   name = "developers"
-# }
-# resource "aws_iam_group_membership" "team" {
-#   name = "developers"
-#   users = [aws_iam_user.bradley.name,
-#     aws_iam_user.marcus.name,
-#     aws_iam_user.minh_vu.name,
-#     aws_iam_user.michelle.name,
-#     aws_iam_user.jennifer.name,
-#   aws_iam_user.elizabeth.name]
+resource "aws_iam_group" "developers" {
+  name = "developers"
+}
+resource "aws_iam_group_membership" "team" {
+  name = "developers"
+  users = [aws_iam_user.bradley.name,
+    aws_iam_user.marcus.name,
+    aws_iam_user.minh_vu.name,
+    aws_iam_user.michelle.name,
+    aws_iam_user.jennifer.name,
+  aws_iam_user.elizabeth.name]
 
-#   group = aws_iam_group.developers.name
-# }
+  group = aws_iam_group.developers.name
+}
 
-# resource "aws_iam_user" "bradley" {
-#   name = "bradley"
-# }
+resource "aws_iam_user" "bradley" {
+  name = "bradley"
+}
 
-# resource "aws_iam_user" "marcus" {
-#   name = "marcus"
-# }
+resource "aws_iam_user" "marcus" {
+  name = "marcus"
+}
 
-# resource "aws_iam_user" "minh_vu" {
-#   name = "minh_vu"
-# }
-# resource "aws_iam_user" "michelle" {
-#   name = "michelle"
-# }
-# resource "aws_iam_user" "jennifer" {
-#   name = "jennifer"
-# }
-# resource "aws_iam_user" "elizabeth" {
-#   name = "elizabeth"
-# }
+resource "aws_iam_user" "minh_vu" {
+  name = "minh_vu"
+}
+resource "aws_iam_user" "michelle" {
+  name = "michelle"
+}
+resource "aws_iam_user" "jennifer" {
+  name = "jennifer"
+}
+resource "aws_iam_user" "elizabeth" {
+  name = "elizabeth"
+}
 
-# resource "aws_iam_policy" "policy" {
-#   name        = "developer-policy"
-#   description = "Policy for all developers in g1t1"
-#   policy      = data.aws_iam_policy_document.developer_policy.json
-# }
+resource "aws_iam_policy" "policy" {
+  name        = "developer-policy"
+  description = "Policy for all developers in g1t1"
+  policy      = data.aws_iam_policy_document.developer_policy.json
+}
 
-# resource "aws_iam_group_policy_attachment" "for-developers" {
-#   group      = aws_iam_group.developers.name
-#   policy_arn = aws_iam_policy.policy.arn
-# }
+resource "aws_iam_group_policy_attachment" "for-developers" {
+  group      = aws_iam_group.developers.name
+  policy_arn = aws_iam_policy.policy.arn
+}
 
 
 # Create AWS IAM Role for Lambda Function
