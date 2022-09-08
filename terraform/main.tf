4c696eb16c350a9cd0e4e2aa5096ee3bf6ef5710
Bradley Yeo <yeo.bradley@gmail.com>
2022-09-08 17:05:00 +0800

    commented out alr deployed resources and it works with a sample react app

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,11 +21,13 @@ resource "aws_iam_role" "amplify-github" {
 }
 
 resource "aws_amplify_app" "Waftech" {
-  name                     = "Waftech"
-  repository               = "https://github.com/cs301-itsa/project-2022-23t1-g1-t1-waffles/tree/fe"
-  access_token             = var.github_token
-  iam_service_role_arn     = aws_iam_role.amplify-github.arn
-  enable_branch_auto_build = true
+  name                        = "Waftech"
+  description                 = "frontend for Waftech"
+  repository                  = "https://github.com/BradleyYeo/react-test"
+  access_token                = var.github_token
+  iam_service_role_arn        = aws_iam_role.amplify-github.arn
+  enable_auto_branch_creation = true
+  enable_branch_auto_build    = true
 
   build_spec = <<-EOT
     version: 0.1
@@ -57,13 +59,13 @@ resource "aws_amplify_app" "Waftech" {
   }
 }
 
-resource "aws_budgets_budget" "under_10_USD" {
-  name         = "Under 10 USD"
-  budget_type  = "COST"
-  limit_amount = "10"
-  limit_unit   = "USD"
-  time_unit    = "MONTHLY"
-}
+# resource "aws_budgets_budget" "under_10_USD" {
+#   name         = "Under 10 USD"
+#   budget_type  = "COST"
+#   limit_amount = "10"
+#   limit_unit   = "USD"
+#   time_unit    = "MONTHLY"
+# }
 
 # Create an archive file for the Python lambda package
 # data "archive_file" "python_lambda_package" {
@@ -72,164 +74,164 @@ resource "aws_budgets_budget" "under_10_USD" {
 #   output_path = "/build/handler.zip"
 # }
 
-resource "aws_iam_group" "developers" {
-  name = "developers"
-}
-resource "aws_iam_group_membership" "team" {
-  name = "developers"
-  users = [aws_iam_user.bradley.name,
-    aws_iam_user.marcus.name,
-    aws_iam_user.minh.name,
-    aws_iam_user.michelle.name,
-    aws_iam_user.jennifer.name,
-  aws_iam_user.elizabeth.name]
+# resource "aws_iam_group" "developers" {
+#   name = "developers"
+# }
+# resource "aws_iam_group_membership" "team" {
+#   name = "developers"
+#   users = [aws_iam_user.bradley.name,
+#     aws_iam_user.marcus.name,
+#     aws_iam_user.minh.name,
+#     aws_iam_user.michelle.name,
+#     aws_iam_user.jennifer.name,
+#   aws_iam_user.elizabeth.name]
 
-  group = aws_iam_group.developers.name
-}
+#   group = aws_iam_group.developers.name
+# }
 
-resource "aws_iam_user" "bradley" {
-  name = "bradley"
-}
+# resource "aws_iam_user" "bradley" {
+#   name = "bradley"
+# }
 
-resource "aws_iam_user" "marcus" {
-  name = "marcus"
-}
+# resource "aws_iam_user" "marcus" {
+#   name = "marcus"
+# }
 
-resource "aws_iam_user" "minh" {
-  name = "minh"
-}
-resource "aws_iam_user" "michelle" {
-  name = "michelle"
-}
-resource "aws_iam_user" "jennifer" {
-  name = "jennifer"
-}
-resource "aws_iam_user" "elizabeth" {
-  name = "elizabeth"
-}
+# resource "aws_iam_user" "minh" {
+#   name = "minh"
+# }
+# resource "aws_iam_user" "michelle" {
+#   name = "michelle"
+# }
+# resource "aws_iam_user" "jennifer" {
+#   name = "jennifer"
+# }
+# resource "aws_iam_user" "elizabeth" {
+#   name = "elizabeth"
+# }
 
-resource "aws_iam_policy" "policy" {
-  name        = "developer-policy"
-  description = "Policy for all developers in g1t1"
-  policy      = data.aws_iam_policy_document.developer_policy.json
-}
+# resource "aws_iam_policy" "policy" {
+#   name        = "developer-policy"
+#   description = "Policy for all developers in g1t1"
+#   policy      = data.aws_iam_policy_document.developer_policy.json
+# }
 
-resource "aws_iam_group_policy_attachment" "for-developers" {
-  group      = aws_iam_group.developers.name
-  policy_arn = aws_iam_policy.policy.arn
-}
+# resource "aws_iam_group_policy_attachment" "for-developers" {
+#   group      = aws_iam_group.developers.name
+#   policy_arn = aws_iam_policy.policy.arn
+# }
 
 
-# Create AWS IAM Role for Lambda Function
-resource "aws_iam_role" "iam_lambda_role" {
-  name = var.lambda_role
+# # Create AWS IAM Role for Lambda Function
+# resource "aws_iam_role" "iam_lambda_role" {
+#   name = var.lambda_role
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Sid    = ""
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Action = "sts:AssumeRole"
+#       Effect = "Allow"
+#       Sid    = ""
+#       Principal = {
+#         Service = "lambda.amazonaws.com"
+#       }
+#     }]
+#   })
+# }
 
-resource "aws_iam_role_policy" "lambda_policy" {
-  name   = "lambda-role-policy"
-  role   = aws_iam_role.iam_lambda_role.id
-  policy = data.aws_iam_policy_document.lambda_policy.json
-}
+# resource "aws_iam_role_policy" "lambda_policy" {
+#   name   = "lambda-role-policy"
+#   role   = aws_iam_role.iam_lambda_role.id
+#   policy = data.aws_iam_policy_document.lambda_policy.json
+# }
 
-# Create S3 Bucket to store different versions of the code
-resource "aws_s3_bucket" "lambda_function_bucket" {
-  bucket        = "lambda-archive-g1t1"
-  force_destroy = var.force_destroy
-}
+# # Create S3 Bucket to store different versions of the code
+# resource "aws_s3_bucket" "lambda_function_bucket" {
+#   bucket        = "lambda-archive-g1t1"
+#   force_destroy = var.force_destroy
+# }
 
-# Create S3 Bucket that accepts files
-resource "aws_s3_bucket" "file_upload_bucket" {
-  bucket        = "file-upload-g1t1"
-  force_destroy = var.force_destroy
-}
+# # Create S3 Bucket that accepts files
+# resource "aws_s3_bucket" "file_upload_bucket" {
+#   bucket        = "file-upload-g1t1"
+#   force_destroy = var.force_destroy
+# }
 
-# Create DynamoDB to store records
-resource "aws_dynamodb_table" "transactions_records_table" {
-  name           = "transaction-records-table"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = "30"
-  write_capacity = "30"
-  hash_key       = "id"
+# # Create DynamoDB to store records
+# resource "aws_dynamodb_table" "transactions_records_table" {
+#   name           = "transaction-records-table"
+#   billing_mode   = "PROVISIONED"
+#   read_capacity  = "30"
+#   write_capacity = "30"
+#   hash_key       = "id"
 
-  attribute {
-    name = "id"
-    type = "S"
-  }
+#   attribute {
+#     name = "id"
+#     type = "S"
+#   }
 
-  ttl {
-    enabled        = true
-    attribute_name = "expiryPeriod"
-  }
-  point_in_time_recovery { enabled = true }
-  server_side_encryption { enabled = true }
-  lifecycle { ignore_changes = [write_capacity, read_capacity] }
-}
+#   ttl {
+#     enabled        = true
+#     attribute_name = "expiryPeriod"
+#   }
+#   point_in_time_recovery { enabled = true }
+#   server_side_encryption { enabled = true }
+#   lifecycle { ignore_changes = [write_capacity, read_capacity] }
+# }
 
-# Create AWS Lambda Function
+# # Create AWS Lambda Function
 
-resource "aws_lambda_function" "file_upload" {
-  function_name    = "test-file-upload"
-  role             = aws_iam_role.iam_lambda_role.arn
-  filename         = "build/handler.zip"                   #data.archive_file.python_lambda_package.output_path
-  source_code_hash = filebase64sha256("build/handler.zip") #data.archive_file.python_lambda_package.output_base64sha256
-  runtime          = "python3.9"
-  memory_size      = 128
-  publish          = true
-  handler          = "handler.handler"
-  timeout          = 100
+# resource "aws_lambda_function" "file_upload" {
+#   function_name    = "test-file-upload"
+#   role             = aws_iam_role.iam_lambda_role.arn
+#   filename         = "build/handler.zip"                   #data.archive_file.python_lambda_package.output_path
+#   source_code_hash = filebase64sha256("build/handler.zip") #data.archive_file.python_lambda_package.output_base64sha256
+#   runtime          = "python3.9"
+#   memory_size      = 128
+#   publish          = true
+#   handler          = "handler.handler"
+#   timeout          = 100
 
-  lifecycle {
-    ignore_changes = [
-      source_code_hash,
-      environment
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       source_code_hash,
+#       environment
+#     ]
+#   }
+# }
 
-resource "aws_lambda_alias" "file_upload_alias" {
-  name             = "production"
-  function_name    = aws_lambda_function.file_upload.arn
-  function_version = "$LATEST"
+# resource "aws_lambda_alias" "file_upload_alias" {
+#   name             = "production"
+#   function_name    = aws_lambda_function.file_upload.arn
+#   function_version = "$LATEST"
 
-  lifecycle {
-    ignore_changes = [
-      function_version
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       function_version
+#     ]
+#   }
+# }
 
-# Create S3 bucket upload trigger for lambda function
+# # Create S3 bucket upload trigger for lambda function
 
-resource "aws_s3_bucket_notification" "file_upload_trigger" {
-  bucket = aws_s3_bucket.file_upload_bucket.id
+# resource "aws_s3_bucket_notification" "file_upload_trigger" {
+#   bucket = aws_s3_bucket.file_upload_bucket.id
 
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.file_upload.arn
-    events              = ["s3:ObjectCreated:*"]
-    #filter_prefix       = "foldername"
-    #filter_suffix       = ".csv"
-  }
+#   lambda_function {
+#     lambda_function_arn = aws_lambda_function.file_upload.arn
+#     events              = ["s3:ObjectCreated:*"]
+#     #filter_prefix       = "foldername"
+#     #filter_suffix       = ".csv"
+#   }
 
-  depends_on = [aws_lambda_permission.s3_permission_to_trigger_lambda]
-}
+#   depends_on = [aws_lambda_permission.s3_permission_to_trigger_lambda]
+# }
 
-resource "aws_lambda_permission" "s3_permission_to_trigger_lambda" {
-  statement_id  = "AllowExecutionFromS3Bucket"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.file_upload.arn
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.file_upload_bucket.arn
-}
+# resource "aws_lambda_permission" "s3_permission_to_trigger_lambda" {
+#   statement_id  = "AllowExecutionFromS3Bucket"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.file_upload.arn
+#   principal     = "s3.amazonaws.com"
+#   source_arn    = aws_s3_bucket.file_upload_bucket.arn
+# }
 
