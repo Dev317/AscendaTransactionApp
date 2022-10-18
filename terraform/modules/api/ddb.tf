af3b54c698e525f6f0d2b0d8c3531e896da5955f
Minhhhh <mineskiroxor@gmail.com>
2022-10-18 15:56:17 +0800

    update db

--- terraform/modules/api/ddb.tf
+++ terraform/modules/api/ddb.tf
@@ -5,6 +5,7 @@ resource "aws_dynamodb_table" "campaign_service_table" {
   #   restore_to_latest_time = true
   billing_mode = "PAY_PER_REQUEST"
   count        = data.aws_region.current.name == var.default_region ? 1 : 0
+  stream_enabled = true
   attribute {
     name = "campaign_id"
     type = "S"
@@ -19,6 +20,8 @@ resource "aws_dynamodb_table" "exclusion_service_table" {
   hash_key     = "exclusion_id"
   billing_mode = "PAY_PER_REQUEST"
   count        = data.aws_region.current.name == var.default_region ? 1 : 0
+  stream_enabled = true
+
   attribute {
     name = "exclusion_id"
     type = "S"
@@ -33,6 +36,8 @@ resource "aws_dynamodb_table" "calculation_service_table" {
   hash_key     = "policy_id"
   billing_mode = "PAY_PER_REQUEST"
   count        = data.aws_region.current.name == var.default_region ? 1 : 0
+  stream_enabled = true
+
   attribute {
     name = "policy_id"
     type = "S"
@@ -48,6 +53,8 @@ resource "aws_dynamodb_table" "transaction_service_table" {
   hash_key     = "transaction_id"
   billing_mode = "PAY_PER_REQUEST"
   count        = data.aws_region.current.name == var.default_region ? 1 : 0
+  stream_enabled = true
+
   attribute {
     name = "transaction_id"
     type = "S"
@@ -62,6 +69,8 @@ resource "aws_dynamodb_table" "reward_service_table" {
   hash_key     = "reward_id"
   billing_mode = "PAY_PER_REQUEST"
   count        = data.aws_region.current.name == var.default_region ? 1 : 0
+  stream_enabled = true
+
   attribute {
     name = "reward_id"
     type = "S"
@@ -76,6 +85,8 @@ resource "aws_dynamodb_table" "card_service_table" {
   hash_key     = "card_type_id"
   billing_mode = "PAY_PER_REQUEST"
   count        = data.aws_region.current.name == var.default_region ? 1 : 0
+  stream_enabled = true
+
   attribute {
     name = "card_type_id"
     type = "S"
@@ -90,6 +101,8 @@ resource "aws_dynamodb_table" "user_card_service_table" {
   hash_key     = "card_id"
   billing_mode = "PAY_PER_REQUEST"
   count        = data.aws_region.current.name == var.default_region ? 1 : 0
+  stream_enabled = true
+
   attribute {
     name = "card_id"
     type = "S"
