2036c0066ba0cb2ed19b51d0b04518cd523d3e47
Minhhhh <mineskiroxor@gmail.com>
2022-11-03 22:36:43 +0800

    added waf

--- terraform/output.tf
+++ terraform/output.tf
@@ -10,60 +10,60 @@ output "dynamodb_table_name" {
 
 output "base_url_campaign_sg" {
   description = "Base URL for API Gateway for Campaign in SG."
-  value = module.api_sg.base_url_campaign
+  value       = module.api_sg.base_url_campaign
 }
 
 output "base_url_campaign_us" {
   description = "Base URL for API Gateway for Campaign in US."
-  value = module.api_north_virginia.base_url_campaign
+  value       = module.api_north_virginia.base_url_campaign
 }
 
 output "base_url_calculation_sg" {
   description = "Base URL for API Gateway for Calculation in SG."
-  value = module.api_sg.base_url_calculation
+  value       = module.api_sg.base_url_calculation
 }
 
 output "base_url_calculation_us" {
   description = "Base URL for API Gateway for Calculation in US."
-  value = module.api_north_virginia.base_url_calculation
+  value       = module.api_north_virginia.base_url_calculation
 }
 
 output "base_url_exclusion_sg" {
   description = "Base URL for API Gateway for Exclusion in SG."
-  value = module.api_sg.base_url_exclusion
+  value       = module.api_sg.base_url_exclusion
 }
 
 output "base_url_exclusion_us" {
   description = "Base URL for API Gateway for Exclusion in US."
-  value = module.api_north_virginia.base_url_exclusion
+  value       = module.api_north_virginia.base_url_exclusion
 }
 
 output "base_url_user_sg" {
   description = "Base URL for API Gateway for User in SG."
-  value = module.api_sg.base_url_user
+  value       = module.api_sg.base_url_user
 }
 
 output "base_url_reward_sg" {
   description = "Base URL for API Gateway for Reward in SG."
-  value = module.api_sg.base_url_reward
+  value       = module.api_sg.base_url_reward
 }
 
 output "base_url_transaction_sg" {
   description = "Base URL for API Gateway for Transaction in SG."
-  value = module.api_sg.base_url_transaction
+  value       = module.api_sg.base_url_transaction
 }
 
 output "base_url_user_us" {
   description = "Base URL for API Gateway for User in US."
-  value = module.api_north_virginia.base_url_user
+  value       = module.api_north_virginia.base_url_user
 }
 
 output "base_url_reward_us" {
   description = "Base URL for API Gateway for Reward in US."
-  value = module.api_north_virginia.base_url_reward
+  value       = module.api_north_virginia.base_url_reward
 }
 
 output "base_url_transaction_us" {
   description = "Base URL for API Gateway for Transaction in US."
-  value = module.api_north_virginia.base_url_transaction
+  value       = module.api_north_virginia.base_url_transaction
 }
